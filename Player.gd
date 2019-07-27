5dd8350ba3a03ca3e72dd9dd9fdea8fb9e606274
Peter Marki <marpeter@protonmail.com>
2019-07-27 14:45:03 +0200

    Introduce a 0.5 sec shot timer in the Player scene
    This limits shot frequency to max 2 shots/sec

--- Player.gd
+++ Player.gd
@@ -5,12 +5,13 @@ signal shoot
 export var speed = 200
 export var max_lives = 3
 var lives
-
+var can_shoot
 var screen_size
 
 func _ready():
     screen_size = get_viewport_rect().size
     lives = max_lives
+    can_shoot = true
 
 func _process(delta):
     var velocity = Vector2()
@@ -18,8 +19,11 @@ func _process(delta):
         velocity.x += 1
     elif Input.is_action_pressed("ui_left"):
         velocity.x -= 1
-    elif Input.is_action_just_pressed("ui_accept"):
+    elif can_shoot and Input.is_action_just_pressed("ui_accept"):
+        # Upon pressing spacebar, tell the Main scene that a shot has happened, then reset the shot timer.
         emit_signal("shoot")
+        can_shoot = false
+        $ShotTimer.start(0.5)
 
     if velocity.length() > 0:
         velocity = velocity.normalized() * speed
@@ -27,3 +31,6 @@ func _process(delta):
     position += velocity * delta
     position.x = clamp(position.x, 0, screen_size.x)
     position.y = clamp(position.y, 0, screen_size.y)
+
+func _on_ShotTimer_timeout():
+    can_shoot = true
